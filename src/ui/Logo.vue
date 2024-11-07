4a9a976190e79ddb9b7648d9c86c65032312f171
Muhammadali0210 <theprogramer0210@gmail.com>
2024-11-07 16:37:49 +0500

    Header devided

---
+++ src/ui/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex items-center justify-between mr-0">
+        <img
+        src="../assets/icons/logo.png"
+        class="mr-3 h-8"
+        alt="Dina Logo"
+        />
+        <span class="self-center max-sm:hidden text-2xl font-semibold whitespace-nowrap dark:text-white">DinaKorean</span>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
