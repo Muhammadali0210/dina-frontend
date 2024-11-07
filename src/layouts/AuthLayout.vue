ec9a23950269031fe81a9f9efd9a923887415bce
Sobitov2002 <sobitov2002@gmail.com>
2024-11-07 08:27:48 +0500

    login

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="  antialiased bg-slate-200 dark:bg-gray-900 flex items-center justify-center min-h-screen">
         <slot />
-
     </div>
 
 </template>
@@ -15,8 +14,6 @@ export default {
         Header,
 
     },
-
-
 }
 </script>
 <style scoped></style>
