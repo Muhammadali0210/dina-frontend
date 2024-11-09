ddf5530d7417f448d2c07c44f326e6cd9c90322d
Sobitov2002 <sobitov2002@gmail.com>
2024-11-09 08:57:54 +0500

    carusel and profil

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -4,18 +4,33 @@
           Mening profilim
         </h1>
     </div>
-    <div class="container flex justify-center">
-        <div class="relative inline-flex items-center justify-center w-[100px] h-[100px] overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-          <span class="font-medium text-gray-600 dark:text-gray-300">AB</span>
-        </div>
+   
+<div href="#" class=" bg-cyan-500 shadow-lg shadow-cyan-500/30 flex w-[100%] flex-col items-center bg-white  md:flex-row rounded-lg shadow   dark:border-green-500 dark:bg-gray-800 ">
+    <img class="object-cover w-full profil-image rounded-t-lg h-66 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="../../assets/icons/user-avatar2.png" alt="">
+    <div 
+     class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white">Ism: <span>Abubakir</span> Familya: <span>Sobitov</span></h5>
+        <p class="mb-3 font-normal text-[20px] text-black dark:text-white">Role: <span class="font-bold">Student</span></p>
+        <p class="mb-3 font-normal text-[20px] text-black dark:text-white">Guruh: <span class="font-bold">Student</span></p>
+        <p class="mb-3 font-normal text-[20px] text-black dark:text-white">Telegram ID: <span class="font-bold">Student</span></p>
     </div>
+    <div>
+        
+    </div>
+</div>
+
+
+
     
 </template>
 <script>
-export default {
 
-}
+
 </script>
-<style lang="">
-
+<style scoped>
+.profil-image {
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
 </style>
