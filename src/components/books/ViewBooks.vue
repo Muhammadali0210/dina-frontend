51af58538dbf1df85ca519f542b5b55b3db7088e
Sobitov2002 <sobitov2002@gmail.com>
2024-11-15 12:01:25 +0500

    a

--- src/components/books/ViewBooks.vue
+++ src/components/books/ViewBooks.vue
@@ -1,14 +1,18 @@
 <template lang="">
-    <div>
-        <iframe width="620px" height="566px" src="https://www.yumpu.com/xx/embed/view/sgJGOf5BkKY4r0AE" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe><br><a href="https://www.yumpu.com/xx/document/view/69119538/soulte-2a-new" title="Soulte 2A new" target="_blank">Soulte 2A new</a>
-    </div>
+<div class="w-full h-screen"  >
+    <div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;max-width:100%;" data-page-width="453" data-page-height="640"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;"   src="https://www.yumpu.com/xx/embed/view/KhD89B27bf5apr1o" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe><br><a href="https://www.yumpu.com/xx/document/view/69119538/soulte-2a-new" title="Soulte 2A new" target="_blank">Soulte 2A new</a></div>
+</div>
+<br>
+<a href="https://www.yumpu.com/xx/document/view/69119538/soulte-2a-new" title="Soulte 2A new" target="_blank">Soulte 2A new</a>
+
+    
 </template>
 <script>
 export default {
     bookData() {
         return {
             bookLink: [
-                { link: 'https://www.yumpu.com/xx/embed/view/elmwvn7WyIhKYFJN' },
+                { link: 'https://www.yumpu.com/xx/embed/view/KhD89B27bf5apr1o' },
             ]
         };
     },
