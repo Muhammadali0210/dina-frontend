ca1e62c4b6e70448ab48f08c79e24066dc5ac1b3
Sobitov2002 <sobitov2002@gmail.com>
2024-11-17 00:47:27 +0500

    dashboard starting

---
+++ src/components/dashboard/Minicard.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container mx-auto p-4">
+        <div class="flex justify-end gap-4" v-if="isAdmin">
+            <div v-for="(cardData, index) in cardsData" :key="index"
+                class="bg-base-100 w-full shadow-xl rounded-lg overflow-hidden">
+                <div class="p-4">
+                    <h2 class="text-lg font-semibold mb-2">{{ cardData.title }}</h2>
+                    <div class="flex justify-between">
+                        <img class="w-[50px] h-[50px]" src="../../assets/icons/groups.svg" alt="card-image">
+                        <h1 class="text-white text-[50px]">20</h1>
+                    </div>
+                    <p class="text-gray-600 text-white">{{ cardData.content }}</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+import { computed } from "vue";
+import { useUserStore } from "@/stores/userStore";
+
+export default {
+    setup() {
+
+        const userStore = useUserStore();
+        const isAdmin = computed(() => userStore.currentRole === "admin")
+        const cardsData = [
+            {
+                title: "Guruhlar soni",
+                content: "If a dog chews s?",
+                buttonText: "Buy Now",
+                src: '../../assets/icons/groups.svg'
+            },
+            {
+                title: "O'quvchilar soni",
+                content: "Lorem ipsum dolor sit ame",
+                buttonText: "Learn More",
+            },
+            {
+                title: "Card 3",
+                content: "Quisquam quae quia nemo,",
+                buttonText: "Subscribe",
+            },
+            {
+                title: "Card 4",
+                content: "Et praesentium ",
+                buttonText: "Read More",
+            },
+        ];
+
+        return {
+            isAdmin,
+            cardsData,
+        };
+    },
+};
+</script>
+
+<style lang="">
+/* Stilni qo'shishingiz mumkin */
+</style>
