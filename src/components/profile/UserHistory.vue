c8b6b0c794b650376c48adda0f7aafd0e06c22ed
Sobitov2002 <sobitov2002@gmail.com>
2024-11-25 01:10:06 +0500

    peymetn start

--- src/components/profile/UserHistory.vue
+++ src/components/profile/UserHistory.vue
@@ -1,13 +1,46 @@
-<template lang="">
-    <div class="mt-[20px] w-full max-w-[1017px] h-auto rounded-[20px] bg-white dark:bg-gray-800 shadow-lg overflow-hidden relative">
+<template>
+    <div
+        class="mt-[20px] w-full max-w-[1017px] h-auto rounded-[20px] bg-white dark:bg-gray-800 shadow-lg overflow-hidden relative">
         <h1 class="text-black m-5 p-[10px] dark:text-white font-bold text-[24px]">To'lov tarixi</h1>
+
+        <div v-for="(payment, index) in peymentData" :key="index"
+            class="flex items-center justify-start p-5 m-[10px] gap-4">
+            <img class="w-[56px] h-[56px]" src="../../assets/icons/peymet.svg" alt="Payment Icon">
+            <div class="flex justify-between w-[90%]">
+                <h1 class="font-bold text-[18px] dark:text-white ">{{ payment.name }}</h1>
+                <h3 class="text-gray-500 dark:text-gray-400 ">{{ payment.date }}</h3>
+                <p class=" " :class="payment.status === 'Complete' ? 'text-green-500' : 'text-red-500'">
+                    {{ payment.status }}
+                </p>
+            </div>
+        </div>
     </div>
 </template>
+
 <script>
 export default {
-
-}
+    data() {
+        return {
+            peymentData: [
+                {
+                    name: 'Abubakir Sobitov',
+                    date: '2022-09-15',
+                    status: 'Complete',
+                },
+                {
+                    name: 'Abdulloh Karimov',
+                    date: '2022-09-16',
+                    status: 'Complete',
+                },
+                {
+                    name: 'Abdulloh Karimov',
+                    date: '2022-09-16',
+                    status: 'Pending',
+                },
+            ],
+        };
+    },
+};
 </script>
-<style lang="">
 
-</style>
+<style scoped></style>
