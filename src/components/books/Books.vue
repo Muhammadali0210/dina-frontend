74fcbecc9bd65b9fede1eb1f7d87f025c4514533
Muhammadali0210 <theprogramer0210@gmail.com>
2024-11-25 13:05:13 +0500

    some changing

--- src/components/books/Books.vue
+++ src/components/books/Books.vue
@@ -1,9 +1,5 @@
 <template lang="">
-    <div >
-        <h1 class="text-3xl font-bold text-green-500">
-            Kitoblar
-        </h1>
-    </div>
+    
     <div class="books-board w-[100%] max-h-[450px] p-[20px] mb-[15px] dark:bg-gray-800 bg-white rounded-lg">
       <StudentBooks/>
     </div>  
