ba52550171dbe85c6e5b832c23caea1f759067e9
Sobitov2002 <sobitov2002@gmail.com>
2024-11-26 22:10:55 +0500

    KOringchi oshna

--- src/components/dashboard/InformationSlider.vue
+++ src/components/dashboard/InformationSlider.vue
@@ -3,47 +3,47 @@
         <div class="flex justify-between mb-[15px]">
             <h1 class="text-[30px] text-gray-800 font-bold dark:text-white">Contunie Watching</h1>
             <div class="gap-3 flex">
-                <button class="bg-white h-[40px] w-[40px] rounded-[50%] relative hover:bg-green-500 "><img
+                <button @click="slidePrev"
+                    class="bg-white h-[40px] w-[40px] rounded-[50%] relative hover:bg-green-500 "><img
                         class="absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]"
                         src="../../assets/icons/arrow-left.svg" alt=""></button>
-                <button class="bg-white h-[40px] w-[40px] rounded-[50%] relative hover:bg-green-500"><img
+                <button @click="slideNext"
+                    class="bg-white h-[40px] w-[40px] rounded-[50%] relative hover:bg-green-500"><img
                         class="absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]"
                         src="../../assets/icons/arrow-right.svg" alt=""></button>
             </div>
         </div>
 
         <div class="bg-white shadow-lg p-[20px] overflow-hidden rounded-[25px] dark:bg-gray-800">
-
-            <swiper :spaceBetween="20" :slidesPerView="3" :freeMode="true" :loop="true" :centeredSlides="true"
-                :autoplay="{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }" :navigation="false" :modules="modules" class="mySwiper">
-                <swiper-slide class="" v-for="(slideData, index) in informationSlideData" :key="index">
+            <swiper :spaceBetween="10" :slidesPerView="1" :loop="true" :autoplay="{
+                delay: 2500,
+                disableOnInteraction: false,
+            }" :breakpoints="{
+                400: { slidesPerView: 1, spaceBetween: 5 },
+                480: { slidesPerView: 2, spaceBetween: 10 },
+                768: { slidesPerView: 3, spaceBetween: 15 },
+                1024: { slidesPerView: 3, spaceBetween: 20 },
+            }" :navigation="false" :modules="modules" class="mySwiper">
+                <swiper-slide v-for="(slideData, index) in informationSlideData" :key="index" class="">
                     <div class="card">
-                        <img class="rounded-[25px]  h-[180px]" :src="slideData.url" alt="">
-                        <!-- <div class="  mt-2 rounded-[35px] bg-[#92d0bc41] px-4 py-[0px]">
-                            <h1 class="flex items-center text-sm font-bold text-[#20d199]">
-                                <img class="w-4" src="../../assets/icons/code.svg" alt="Icon">{{ slideData.title }}
-                            </h1>
-                        </div> -->
-                        <p class="text-gray-800 dark:text-white text-lg font-bold leading-[23px] break-words pt-[15px]">
+                        <img class="rounded-[15px] h-[150px] md:h-[180px] lg:h-[200px] w-full object-cover"
+                            :src="slideData.url" alt="" />
+                        <p
+                            class="text-gray-800 dark:text-white text-sm md:text-base lg:text-lg font-bold leading-[23px] break-words pt-[10px]">
                             {{ slideData.description }}
                         </p>
                     </div>
                 </swiper-slide>
-
             </swiper>
-
         </div>
+
     </div>
 
 </template>
 <script>
 import { Swiper, SwiperSlide } from 'swiper/vue';
 
-// Import Swiper styles
-import 'swiper/css';
+
 
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -84,6 +84,14 @@ export default {
             ]
         };
     },
+    // methods: {
+    //     slidePrev() {
+    //         this.swiperInstance.slidePrev();
+    //     },
+    //     slideNext() {
+    //         this.swiperInstance.slideNext();
+    //     }
+    // }
 
 }
 </script>
