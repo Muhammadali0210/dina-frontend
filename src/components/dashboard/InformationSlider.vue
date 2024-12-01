80d5dab3aee32f42265e0e2de84a34b7f2ee4221
Sobitov2002 <sobitov2002@gmail.com>
2024-12-01 12:08:37 +0500

    z

--- src/components/dashboard/InformationSlider.vue
+++ src/components/dashboard/InformationSlider.vue
@@ -1,33 +1,42 @@
 <template>
-    <div class=" h-full p-[0px]">
+    <div class="h-full p-[0px]">
         <div class="flex justify-between mb-[15px]">
-            <h1 class="text-[30px] text-gray-800 font-bold dark:text-white">Contunie Watching</h1>
+            <h1 class="text-[30px] text-gray-800 font-bold dark:text-white">Continue Watching</h1>
             <div class="gap-3 flex">
-                <button @click="slidePrev"
-                    class="bg-white h-[40px] w-[40px] rounded-[50%] relative hover:bg-green-500 "><img
-                        class="absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]"
-                        src="../../assets/icons/arrow-left.svg" alt=""></button>
-                <button @click="slideNext"
-                    class="bg-white h-[40px] w-[40px] rounded-[50%] relative hover:bg-green-500"><img
-                        class="absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]"
-                        src="../../assets/icons/arrow-right.svg" alt=""></button>
+                <!-- Oldingi slayd tugmasi -->
+                <button @click="slidePrev" class="bg-white h-[40px] w-[40px] rounded-[50%] relative hover:bg-green-500">
+                    <img class="absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]"
+                        src="/src/assets/icons/arrow-left.svg" alt="Prev">
+                </button>
+                <!-- Keyingi slayd tugmasi -->
+                <button @click="slideNext" class="bg-white h-[40px] w-[40px] rounded-[50%] relative hover:bg-green-500">
+                    <img class="absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]"
+                        src="/src/assets/icons/arrow-right.svg" alt="Next">
+                </button>
             </div>
         </div>
 
         <div class="bg-white shadow-lg p-[20px] overflow-hidden rounded-[25px] dark:bg-gray-800">
-            <swiper :spaceBetween="10" :slidesPerView="1" :loop="true" :autoplay="{
-                delay: 2500,
-                disableOnInteraction: false,
-            }" :breakpoints="{
+            <!-- Swiper komponenti -->
+            <swiper ref="swiperRef" :onSwiper="onSwiperInit" 
+                :spaceBetween="10"
+                :slidesPerView="1"
+                :loop="true"
+                :autoplay="{ delay: 2500, disableOnInteraction: false }"
+                :breakpoints="{
                 400: { slidesPerView: 1, spaceBetween: 5 },
                 480: { slidesPerView: 2, spaceBetween: 10 },
                 768: { slidesPerView: 3, spaceBetween: 15 },
-                1024: { slidesPerView: 3, spaceBetween: 20 },
-            }" :navigation="false" :modules="modules" class="mySwiper">
-                <swiper-slide v-for="(slideData, index) in informationSlideData" :key="index" class="">
+                1024: { slidesPerView: 3, spaceBetween: 20 }
+                }"
+                :modules="modules"
+                class="mySwiper"
+                >
+                <!-- Swiper slaydlari -->
+                <swiper-slide v-for="(slideData, index) in informationSlideData" :key="index">
                     <div class="card">
                         <img class="rounded-[15px] h-[150px] md:h-[180px] lg:h-[200px] w-full object-cover"
-                            :src="slideData.url" alt="" />
+                            :src="slideData.url" alt="Image" />
                         <p
                             class="text-gray-800 dark:text-white text-sm md:text-base lg:text-lg font-bold leading-[23px] break-words pt-[10px]">
                             {{ slideData.description }}
@@ -38,13 +47,13 @@
         </div>
     </div>
 </template>
+
 <script>
 import { Swiper, SwiperSlide } from 'swiper/vue';
-
-
-
+import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import { ref } from 'vue';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 export default {
@@ -53,46 +62,47 @@ export default {
         SwiperSlide,
     },
     setup() {
+        const swiperRef = ref(null);
+
+        const onSwiperInit = (swiper) => {
+            swiperRef.value = swiper;
+            console.log("Swiper is ready!");
+        };
+
+        const slideNext = () => {
+            if (swiperRef.value) {
+                swiperRef.value.slideNext();
+            } else {
+                console.warn("Swiper is not ready yet!");
+            }
+        };
+
+        const slidePrev = () => {
+            if (swiperRef.value) {
+                swiperRef.value.slidePrev();
+            } else {
+                console.warn("Swiper is not ready yet!");
+            }
+        };
+
         return {
+            swiperRef,
+            onSwiperInit,
+            slidePrev,
+            slideNext,
             modules: [Autoplay, Pagination, Navigation],
             informationSlideData: [
-                {
-                    url: './src/assets/image/inf.jpg',
-                    title: 'FRONT-END',
-                    description: 'Beginner\'s Guide to Becoming a ',
-                    image: './src/assets/image/inf.jpg',
-                },
-                {
-                    url: './src/assets/image/inf3.jpg',
-                    title: 'UI/UX DESIGN',
-                    description: 'Optimizing User Expriendence ',
-                    image: './src/assets/image/inf.jpg',
-                }, {
-                    url: './src/assets/image/inf.jpg',
-                    title: 'FRONT-END',
-                    description: 'Beginner\'s Guide to Becoming a ',
-                    image: './src/assets/image/inf.jpg',
-                },
-                {
-                    url: './src/assets/image/inf3.jpg',
-                    title: 'UI/UX DESIGN',
-                    description: 'Optimizing User Expriendence ',
-                    image: './src/assets/image/inf.jpg',
-                },
-            ]
+                { url: '/src/assets/image/inf.jpg', description: 'Beginner\'s Guide to Becoming a Front-End Developer' },
+                { url: '/src/assets/image/inf3.jpg', description: 'Optimizing User Experience' },
+                { url: '/src/assets/image/inf.jpg', description: 'Mastering Front-End Skills' },
+                { url: '/src/assets/image/inf3.jpg', description: 'Perfecting the User Journey' },
+            ],
         };
-    },
-    // methods: {
-    //     slidePrev() {
-    //         this.swiperInstance.slidePrev();
-    //     },
-    //     slideNext() {
-    //         this.swiperInstance.slideNext();
-    //     }
-    // }
+    }
 
-}
+};
 </script>
+
 <style>
 .box {
     color: #06f5a9;
