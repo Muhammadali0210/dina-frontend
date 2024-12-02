f296a71e5ecd34d97d8256d809b258051e7d58bf
Sobitov2002 <sobitov2002@gmail.com>
2024-12-02 22:00:20 +0500

    sasa

---
+++ src/components/onlinecourse/OnlineCourse.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <div class="text-gray-800 dark:text-white" >
+        Video Kurs
+    </div>
+    <div>
+    <iframe class="w-[70%] h-[70vh] m-auto" src="https://www.youtube.com/embed/lXGFKMFTQEg?si=724h4lDvqAQL86qi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <h1 class="text-center text-gray-700 text-[20px] dark:text-white ">KOREYS TILINI QAYSI KITOBDAN O’RGANISH YAXSHI?🇰🇷</h1>
+        <button @click="nextClick" class="p-[10px] bg-green-500 dark:text-white"  type="">Keyingisi...</button>
+    </div>
+
+</template>
+<script>
+export default {
+    methods: {
+        nextClick(){
+            this.$router.push('/onlinevideo')
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/onlinecourse/CourseView.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <OnlineCourse/>
+    </div>
+</template>
+<script>
+import OnlineCourse from '@/components/onlinecourse/OnlineCourse.vue'
+export default {
+    components:{
+        OnlineCourse
+    }
+}
+</script>
+<style lang="">
+    
+</style>
