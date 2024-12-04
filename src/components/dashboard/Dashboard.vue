c326c425a041e0f0a29c09bee32dcf77e5af1202
Muhammadali0210 <theprogramer0210@gmail.com>
2024-12-04 08:45:53 +0500

    dashboard container

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class=" grid grid-cols-3 gap-[25px] w-full max-w-[1300px] border-spacing-2 max-xl:grid-cols-1">
+  <div class="custom-container grid grid-cols-3 gap-[25px] w-full max-w-[1300px] border-spacing-2 max-xl:grid-cols-1">
     <main class="col-span-3 grid grid-cols-1 gap-4">
       <Banner />
       <Statistic />
