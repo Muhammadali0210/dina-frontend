b185225a5fb0590470f48e3c5ada6314fd9727b2
Sobitov2002 <Sobitov2002@gmail.com>
2024-12-13 15:46:23 +0500

    lesson

---
+++ src/components/onlinecourse/ui/AccordionContentLesson.vue
@@ -0,0 +1,14 @@
+<script  lang="ts" setup>
+defineProps({
+    data: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+<template lang="">
+   
+</template>
+<style lang="">
+    
+</style>
--- src/components/onlinecourse/ui/LessonSaidbar.vue
+++ src/components/onlinecourse/ui/LessonSaidbar.vue
@@ -1,7 +1,8 @@
 <script>
 import LessonModul from '../ui/LessonModul.vue'
 import { useSidebarStore } from '@/stores/sidebarStore';
-import Progress from '../ui/Progress.vue'
+import { Progress } from '@/components/ui/progress'
+
 
 export default {
     setup() {
@@ -15,13 +16,48 @@ export default {
     },
     components: {
         LessonModul,
+        Progress
+    },
+    data(){
+        return{
+             accordionItems: [
+                {
+                    value: 'item-1',
+                    title: '1 - Modul',
+                    lessonData: [
+                        { id: '1', title: '#1 - Dars' },
+                        { id: '2', title: '#2 - Dars' },
+                       
+                    ],
+                },
+                {
+                    value: 'item-2',
+                    title: '2 - Modul',
+                    lessonData: [
+                        { id: '4', title: '#4 - Dars' },
+                       
+                    ],
+                },
+                {
+                    value: 'item-3',
+                    title: '3 - Modul',
+                    lessonData: [
+                        { id: '7', title: '#7 - Dars' },
+                        { id: '8', title: '#8 - Dars' },
+                        { id: '9', title: '#9 - Dars' },
+                        { id: '9', title: '#9 - Dars' },
+                    ],
+                },
+            ]
+
+        }
     }
 }
 </script>
 <template>
     <main :class="{ 'translate-x-0': sidebarStore.isOpen }"
-        class="fixed top-0 left-0 z-40 w-80 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 lg:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
-        <Progress/>
-        <LessonModul />
+        class="fixed top-0 left-0 z-40 w-80 h-screen pt-4 transition-transform -translate-x-full  border-r border-gray-200 lg:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
+        <Progress :model-value="33"/>
+        <LessonModul :data="accordionItems" />
     </main>
 </template>
