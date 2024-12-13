7d8ea75cf83cec9dce814c4358343c05e02341d4
Sobitov2002 <Sobitov2002@gmail.com>
2024-12-13 15:48:34 +0500

    lesson

--- src/components/onlinecourse/CourseLessonFull.vue
+++ src/components/onlinecourse/CourseLessonFull.vue
@@ -1,12 +1,12 @@
 <template lang="">
     <div class="">
-       <LessonVideo/>
+       
     </div>
 </template>
 <script>
-import LessonVideo from '@/components/onlinecourse/ui/Progress.vue'
+
 export default {
-    LessonVideo,
+  
 }
 </script>
 <style lang="">
---
+++ src/components/onlinecourse/ui/VideoComponent.vue
@@ -0,0 +1,38 @@
+<script>
+import { Button } from '@/components/ui/button'
+
+export default {
+  props: {
+    videoUrl: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    nextVideo() {
+      this.$emit('next-video'); // Emit an event to go to the next video
+    }
+  }
+};</script>
+
+<template>
+  <div class="w-full ml-6 md:ml-0 m-auto">
+    <div style="padding: 56.25% 0 0 0; position: relative">
+      <iframe
+        :src="videoUrl"
+        frameborder="0"
+        allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
+        title="TOPIKGA_TOLOV_AMALGA_OSHIRISH__#dinakorean_#topik_#korean_#koreystili"
+        @ended="nextVideo"
+      ></iframe>
+    </div>
+    <div class="flex justify-between mt-3">
+      <Button class="px-[15px] py-[5px] bg-blue-800 rounded-lg" @click="nextVideo">Keyingi Dars</Button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Styles if needed */
+</style>
