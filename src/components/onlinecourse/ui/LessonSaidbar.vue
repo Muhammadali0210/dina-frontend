4e9015a2629371376a3125afd55b9e0bdb6b5cab
Sobitov2002 <Sobitov2002@gmail.com>
2024-12-16 23:06:46 +0500

    Qaleysiz oshna

--- src/components/onlinecourse/ui/LessonSaidbar.vue
+++ src/components/onlinecourse/ui/LessonSaidbar.vue
@@ -57,7 +57,7 @@ export default {
 <template>
     <main :class="{ 'translate-x-0': sidebarStore.isOpen }"
         class="fixed top-0 left-0 z-40 w-80 h-screen pt-4 transition-transform -translate-x-full  border-r border-gray-200 lg:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
-        <Progress :model-value="33"/>
+        <Progress :model-value="43" />
         <LessonModul :data="accordionItems" />
     </main>
 </template>
