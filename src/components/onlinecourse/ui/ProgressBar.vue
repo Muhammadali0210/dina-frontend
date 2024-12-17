4cc9c22674cb6d45dba4781bb3eaa9b252394a72
Muhammadali0210 <theprogramer0210@gmail.com>
2024-12-17 12:37:37 +0500

    Acardion and progress

--- src/components/onlinecourse/ui/ProgressBar.vue
+++ src/components/onlinecourse/ui/ProgressBar.vue
@@ -1,17 +1,14 @@
+<script setup lang="ts">
+import { Progress } from '@/components/ui/progress'
+
+
+</script>
 <template lang="">
-   <div class="mt-14 lg:mt-0 px-[10px] mb-[10px] pb-[7px] border-b border-gray-600">
-    <h1 class="dark:text-white text-[14px]">Abubakir Sobitov</h1>
-     <Progress class="" :model-value="43" />
+   <div class="lg:mt-0 px-2 py-[11px] space-y-1  border-b border-gray-600">
+    <div class="flex justify-between">
+      <h1 class="dark:text-white text-[18px] font-bold leading-[18px]">O'zlashtirish</h1>
+      <h1 class="dark:text-white text-[18px] font-bold leading-[18px]">64%</h1>
+    </div>
+      <Progress class="" :model-value="64" /> 
    </div>
 </template>
-<script>
-import { Progress } from '@/components/ui/progress'
-export default {
-    components: {
-        Progress
-},
-}
-</script>
-<style lang="">
-    
-</style>
