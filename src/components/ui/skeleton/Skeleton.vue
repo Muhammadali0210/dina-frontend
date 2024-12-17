12330e823c7f9df62416ad835baef45eee540ea4
Muhammadali0210 <theprogramer0210@gmail.com>
2024-12-17 11:03:42 +0500

    Course card skleton

---
+++ src/components/ui/skeleton/Skeleton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+interface SkeletonProps {
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<SkeletonProps>()
+</script>
+
+<template>
+  <div :class="cn('animate-pulse rounded-md bg-slate-300 dark:bg-gray-700', props.class)" />
+</template>
