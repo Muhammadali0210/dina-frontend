3d3ce17bf5df416d5da496a2d688ab011909b475
Muhammadali0210 <theprogramer0210@gmail.com>
2024-12-25 00:08:54 +0500

    some changing with test

--- src/pages/course-instructor/update-page/CourseInstructorUpdate.vue
+++ src/pages/course-instructor/update-page/CourseInstructorUpdate.vue
@@ -6,6 +6,10 @@ import { useRoute } from "vue-router";
 import TitleField from "./_components/TitleField.vue";
 import DescriptionField from "./_components/DescriptionField.vue";
 import InformationField from "./_components/InformationField.vue";
+import SelectFields from "./_components/SelectFields.vue";
+import SectionsField from "./_components/SectionsField.vue";
+import ImagesField from "./_components/ImagesField.vue";
+import PriceField from "./_components/PriceField.vue";
 const router = useRoute();
 import { useGetCourseInfo } from "@/pages/course-instructor/update-page/service";
 
@@ -28,20 +32,37 @@ onMounted( async () => {
 <template>
   <PageContainer
     :title="isLoading ? 'Loading...' : courseData ? courseData.title : 'Kurs nomi bo\'sh'"
-    :subtitle="isLoading ? 'Loading...' : courseData ? courseData.description : 'Kurs malumoti bo\'sh'"
+    subtitle="Kursingizni malumotlarini boshqaring va u qanday tekshiring"
   >
-    <div class="grid grid-cols-2 gap-4">
+    <div class="grid grid-cols-2 max-md:grid-cols-1 gap-4">
       <div class="space-y-3">
-        <CourseFieldCard name="Kurst nomi" #default="{ state }" :isUpdated="isUpdated">
+        <CourseFieldCard name="Nomi" #default="{ state }" :isUpdated="isUpdated">
           <TitleField :state="state" @onUpdated="onUpdate" :course="courseData" />
         </CourseFieldCard>
 
-        <CourseFieldCard name="Qisqacha malumot" #default="{ state }" :isUpdated="isUpdated">
+        <CourseFieldCard name="Kurs haqida" #default="{ state }" :isUpdated="isUpdated">
           <DescriptionField :state="state" @onUpdated="onUpdate" :course="courseData" />
         </CourseFieldCard>
 
-        <CourseFieldCard name="Kurs nimalarni o'rgatadi?" #default="{ state }" :isUpdated="isUpdated">
+        <CourseFieldCard name="Kurs malumotlari" #default="{ state }" :isUpdated="isUpdated">
           <InformationField :state="state" @onUpdated="onUpdate" :course="courseData" />
+        </CourseFieldCard>
+
+        <CourseFieldCard name="Kurs malumotlari" #default="{ state }" :isUpdated="isUpdated">
+          <SelectFields :state="state" @onUpdated="onUpdate" :course="courseData" />
+        </CourseFieldCard>
+      </div>
+      <div class="space-y-3">
+        <CourseFieldCard name="Bo'limlar" #default="{ state }" :isUpdated="isUpdated">
+          <SectionsField :state="state" @onUpdated="onUpdate" :course="courseData" />
+        </CourseFieldCard>
+
+        <CourseFieldCard name="Narx" #default="{ state }" :isUpdated="isUpdated">
+          <PriceField :state="state" @onUpdated="onUpdate" :course="courseData" />
+        </CourseFieldCard>
+
+        <CourseFieldCard name="Rasm" #default="{ state }" :isUpdated="isUpdated">
+          <ImagesField :state="state" @onUpdated="onUpdate" :course="courseData" />
         </CourseFieldCard>
       </div>
     </div>
