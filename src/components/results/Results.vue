04fce11fe2dc38ce335585de9591adb82e10889b
Sobitov2002 <Sobitov2002@gmail.com>
2025-01-03 19:45:51 +0500

    loading="lazy" add

--- src/components/results/Results.vue
+++ src/components/results/Results.vue
@@ -19,13 +19,15 @@ data.value = resultData
             <div v-for="(result, index) in data" :key="index"
                 class="h-[350px]  max-sm:h-[240px]  group relative rounded-xl overflow-hidden card w-full ">
 
-                <img class="" :src="result.link" alt="">
-                class="h-[350px] max-sm:h-[300px] border border-border group relative rounded-xl overflow-hidden card w-full ">
-                
-            class="h-[350px] max-sm:h-[300px] border border-border group relative rounded-xl overflow-hidden card w-full ">
+                <img class="" :src="result.link" alt="" loading="lazy">
+                class="h-[350px] max-sm:h-[300px] border border-border group relative rounded-xl overflow-hidden card
+                w-full ">
+
+                class="h-[350px] max-sm:h-[300px] border border-border group relative rounded-xl overflow-hidden card
+                w-full ">
 
 
-                <img :src="result.link" alt="result image" class="w-full h-full object-cover" >
+                <img :src="result.link" alt="result image" class="w-full h-full object-cover" loading="lazy">
                 <Dialog class="p-0">
                     <DialogTrigger
                         class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-blue-600 text-white text-sm px-5 py-3 rounded-lg shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300">
@@ -35,7 +37,7 @@ data.value = resultData
                         <DialogTitle class="sr-only">Rasmni ko'rish</DialogTitle>
                         <DialogDescription class="sr-only">
                         </DialogDescription>
-                        <img class="p-0" :src="result.link" alt="result image">
+                        <img class="p-0" :src="result.link" alt="result image" loading="lazy">
                     </DialogContent>
                 </Dialog>
             </div>
