ffca2ffae7cc37b2e8721e6d1b037803d2806bd1
Sobitov2002 <Sobitov2002@gmail.com>
2025-01-03 19:22:59 +0500

    dropdown update

--- src/components/ui/dropdown-menu/DropdownMenuItem.vue
+++ src/components/ui/dropdown-menu/DropdownMenuItem.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { cn } from '@/utils'
+import { cn } from '@/lib/utils'
 import { DropdownMenuItem, type DropdownMenuItemProps, useForwardProps } from 'radix-vue'
 import { computed, type HTMLAttributes } from 'vue'
 
@@ -18,7 +18,7 @@ const forwardedProps = useForwardProps(delegatedProps)
   <DropdownMenuItem
     v-bind="forwardedProps"
     :class="cn(
-      'relative flex cursor-default select-none items-center rounded-sm gap-2 px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50  [&>svg]:size-4 [&>svg]:shrink-0',
+      'relative flex cursor-pointer select-none items-center rounded-sm gap-2 px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50  [&>svg]:size-4 [&>svg]:shrink-0',
       inset && 'pl-8',
       props.class,
     )"
