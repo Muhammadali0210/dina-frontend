e43b22f260578fdd902d7b567924a1633f99c544
Muhammadali0210 <theprogramer0210@gmail.com>
2025-01-04 21:37:18 +0500

    sidebar Loader created

---
+++ src/pages/(lesson-dashboard)/_components/Loading.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Skeleton } from '@/components/ui/skeleton'
+</script>
+  
+<template>
+    <div class="w-full">
+        <div class="h-[10vh] w-full px-2 py-1 space-y-1  border-b border-gray-600">
+            <Skeleton class="h-4 mt-1 w-[70%] " />
+            <Skeleton class="h-4 w-full bg-gray-400 rounded-none" />
+            <h1 class=" text-sm font-medium">0% Tugadi</h1>
+        </div>
+        <div class="mx-auto mt-1 flex w-[98%] flex-col space-y-1">
+            <div v-for="i in 4" :key="i" class="bg-slate-300 dark:bg-gray-900 flex h-12 items-center justify-between px-2 py-4">
+                <div class="flex flex-1 items-center space-x-2">
+                    <Skeleton class="h-4 w-4 bg-blue-400" />
+                    <Skeleton class="h-4 w-1/3 bg-blue-400" />
+                </div>
+                <Skeleton class="h-4 w-4 bg-blue-400" />
+            </div>
+        </div>
+    </div>
+</template>
+  
+  
--- src/pages/(lesson-dashboard)/_components/Sidebar.vue
+++ src/pages/(lesson-dashboard)/_components/Sidebar.vue
@@ -1,20 +1,43 @@
 <script setup lang="ts">
 import Section from './Section.vue';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import { Progress } from '@/components/ui/progress'
 import { useSidebarStore } from '@/stores/sidebarStore';
+import { useGetDashboardCourse } from '../service';
+import Loading from './Loading.vue';
+import { Button } from "@/components/ui/button";
+import { Download } from 'lucide-vue-next';
 
 const sidebarStore = useSidebarStore();
+const route = useRoute();
+const { isLoading, course, getDashboardCourse } = useGetDashboardCourse();
+
+const reload = async() => {
+    await getDashboardCourse(Number(route.params.id));
+}
+onMounted(async() => {
+    await getDashboardCourse(Number(route.params.id));
+    console.log(course.value);
+})
 </script>
 <template>
     <main :class="{ 'translate-x-0': sidebarStore.isOpen }"
         class="fixed top-0 left-0 z-40 w-80 h-screen pt-0 transition-transform -translate-x-full bg-white  border-r border-gray-200 lg:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
-        <div class="lg:mt-0 px-2 py-[11px] space-y-1  border-b border-gray-600">
-            <div class="flex justify-between">
-            <h1 class="dark:text-white text-[18px] font-bold leading-[18px]">O'zlashtirish</h1>
-            <h1 class="dark:text-white text-[18px] font-bold leading-[18px]">64%</h1>
+        <template v-if="isLoading"><Loading/></template>
+        <template v-else>
+            <template v-if="course">
+                <div class="px-2 py-1 space-y-1  border-b border-gray-600">
+                    <h1 class="font-bold">{{ course?.course.title }}</h1>
+                    <Progress class="rounded-none bg-gray-200 dark:bg-gray-700" :model-value="course?.progressPercentage" /> 
+                    <h1 class=" text-sm font-medium">{{course?.progressPercentage}}% Tugadi</h1>
+                </div>
+                <Section :sections="course?.sections" />
+            </template>
+            <div v-else class="flex flex-col h-full items-center justify-center">
+                <h1 class="text-center mb-3">Internet bilan bog'liq muommo mavjud. Malumotlar yuklanmadi!</h1>
+                <Button @click="reload"><Download />Qayta yuklash</Button>
             </div>
-            <Progress class="" :model-value="64" /> 
-        </div>
-        <Section />
+        </template>
     </main>
 </template>
