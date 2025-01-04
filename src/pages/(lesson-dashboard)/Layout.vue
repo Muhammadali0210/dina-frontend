dfae15f9dd8fb8490d48aecfb04b180d54fd77db
Muhammadali0210 <theprogramer0210@gmail.com>
2025-01-04 18:18:26 +0500

    lesson dashboard created

---
+++ src/pages/(lesson-dashboard)/Layout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Navbar from './_components/Navbar.vue';
+import Sidebar from './_components/Sidebar.vue'
+import { RouterView } from 'vue-router';
+import { useSidebarStore } from '@/stores/sidebarStore';
+
+const sidebarStore = useSidebarStore();
+</script>
+
+<template>
+    <div class="antialiased bg-slate-200 dark:bg-gray-900 ">
+        <div class="grid grid-cols-2">
+            <Navbar/>
+            <Sidebar/>
+        </div>
+        <main class=" p-0  pt-20  lg:ml-80 min-h-screen" @click="sidebarStore.closeSidebar">
+            <h1>Video Player</h1>
+            <!-- <RouterView /> -->
+        </main>
+    </div>
+</template>
+
---
+++ src/pages/(lesson-dashboard)/_components/Sidebar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Section from './Section.vue';
+import { Progress } from '@/components/ui/progress'
+import { useSidebarStore } from '@/stores/sidebarStore';
+
+const sidebarStore = useSidebarStore();
+</script>
+<template>
+    <main :class="{ 'translate-x-0': sidebarStore.isOpen }"
+        class="fixed top-0 left-0 z-40 w-80 h-screen pt-0 transition-transform -translate-x-full bg-white  border-r border-gray-200 lg:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
+        <div class="lg:mt-0 px-2 py-[11px] space-y-1  border-b border-gray-600">
+            <div class="flex justify-between">
+            <h1 class="dark:text-white text-[18px] font-bold leading-[18px]">O'zlashtirish</h1>
+            <h1 class="dark:text-white text-[18px] font-bold leading-[18px]">64%</h1>
+            </div>
+            <Progress class="" :model-value="64" /> 
+        </div>
+        <Section />
+    </main>
+</template>
