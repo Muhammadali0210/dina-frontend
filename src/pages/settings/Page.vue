9c65c6944fceab3bf7ebfa7ea94fc26c1445be28
Sobitov2002 <Sobitov2002@gmail.com>
2025-01-10 14:23:34 +0500

    upgrade

--- src/pages/settings/Page.vue
+++ src/pages/settings/Page.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-import PageContainer from '@/components/PageContainer.vue'
-import PasswordStepper from './_components/PasswordStepper.vue'
-import DarkMode from '@/ui/DarkMode.vue'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import PageContainer from '@/components/PageContainer.vue';
+import PasswordStepper from './_components/PasswordStepper.vue';
+
 </script>
 
 <template>
   <PageContainer class="" title="Sozlamalar" subtitle="Dastur uchun sozlamalar bo'limi">
-
-    <div class="flex justify-center min-h-[70vh] items-center  ">
+    <div class="flex justify-center min-h-[70vh] items-center">
       <PasswordStepper />
     </div>
   </PageContainer>
-
 </template>
