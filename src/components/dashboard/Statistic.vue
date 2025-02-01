4c9608de2d476d51f53fc36f12f8e88921dbbc5a
Sobitov2002 <sobitov2002@gmail.com>
2025-02-01 11:47:17 +0500

    sa

--- src/components/dashboard/Statistic.vue
+++ src/components/dashboard/Statistic.vue
@@ -10,7 +10,7 @@
             </div>
             <div class="">
                 <p class="font-bold dark:text-gray-300 text-gray-500">O'quvchilar</p>
-                <h1 class="text-[35px] max-md:text-[30px] text-gray-700 dark:text-white leading-none font-extrabold">148
+                <h1 class="text-[35px] max-md:text-[30px] text-gray-700 dark:text-white leading-none font-extrabold">100
                 </h1>
             </div>
         </div>
